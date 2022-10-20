3def137604e086a25c6e0f6e578f26532a0330d2
Jeivisson Lopes <jeivisson@Jeivisson.localdomain>
2022-10-20 18:20:37 -0300

    door events done!

---
+++ src/components/Porta/Porta.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import PortaModel from '../../model/porta';
+import styles from './porta.module.css';
+
+interface PortaProps {
+  value: PortaModel
+  onChange: (novaPorata: PortaModel) => void
+}
+
+export default function Porta(props : PortaProps) {
+  const  porta  = props.value
+  const selecionada = porta.selecionada && !porta.aberta ? styles.selecionada : '';
+
+  const alternarSelecao = e => props.onChange(porta.alternarSelecao())
+  const abrir = e => {
+    e.stopPropagation()
+    props.onChange(porta.abrir())
+
+  }
+  function renderizarPorta() {
+
+    return (
+      <div className={styles.porta}>
+      <div className={styles.numero}>{porta.numero}</div>
+      <div className={styles.macaneta} onClick = {abrir}></div>
+    </div>
+  )
+
+}
+return (
+  <div className={styles.area} onClick={alternarSelecao}>
+      <div className={`${styles.estrutura} ${selecionada}`}>
+        {porta.aberta ? false : renderizarPorta() }
+      </div>
+        <div className={styles.chao}></div>
+  </div>
+  );
+}
